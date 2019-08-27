d36ea5be46688f5197e40d356fc471b0e944bd7c
carbotaniuman <41451839+carbotaniuman@users.noreply.github.com>
2019-08-27 12:18:40 -0500

    Drive with joystick

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
-import serial
+from tkinter import *
+
+from time import sleep
+from serial import Serial
+
+ser = Serial("COM4")
+
+
+def run():
+    ser.write(str(w1.get()).encode() + b"\n")
+    ser.write(str(w2.get()).encode() + b"\n")
+    master.after(100, run)
+
+
+master = Tk()
+w1 = Scale(master, from_=100, to=-100)
+w1.set(0)
+w1.pack()
+w2 = Scale(master, from_=-100, to=100,  orient=HORIZONTAL)
+w2.set(0)
+w2.pack()
+
+master.after(100, run)
+
+mainloop()
