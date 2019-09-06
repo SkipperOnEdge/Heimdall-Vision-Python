cf38fd0f14c9be5844de2d558bcb97762cc45edb
Luca Macesanu <53663808+SkipperOnEdge@users.noreply.github.com>
2019-09-06 12:20:26 -0500

    Vision Update
    Got Contours and HSV Display working, now need to filter countours to find center and then pass taht to determine motor funciton.

--- main.py
+++ main.py
@@ -1,9 +1,36 @@
 from tkinter import *
-
-from time import sleep
 from serial import Serial
+from grip import GripPipeline
+import numpy as np
+import cv2
 
+#INITIALIZATION
+cap = cv2.VideoCapture(0)
+grip = GripPipeline()
+
+
+#VISION LOOP
+while(True):
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+    # Our operations on the frame come here
+
+    # Grip Pipeline
+    grip.process(frame)
+
+    # Display the resulting frame
+    cv2.imshow('frame', frame)
+
+    # Press q to break
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
 ser = Serial("COM4")
+
+
 
 
 def run():
@@ -12,6 +39,7 @@ def run():
     master.after(100, run)
 
 
+# Joystick Code
 master = Tk()
 w1 = Scale(master, from_=100, to=-100)
 w1.set(0)
