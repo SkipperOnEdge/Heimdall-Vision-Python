ae2694a936994548deed3e96530077813d16344e
Luca Macesanu <53663808+SkipperOnEdge@users.noreply.github.com>
2019-09-09 12:37:20 -0500

    Find Center of Image
    Implemented code that ID's the center of the pencil bag

--- grip.py
+++ grip.py
@@ -76,9 +76,23 @@ class GripPipeline:
                                                                self.__filter_contours_min_vertices,
                                                                self.__filter_contours_min_ratio,
                                                                self.__filter_contours_max_ratio)
-        if(self.filter_contours_output):
-            print(self.filter_contours_output[0][0])
-            print("-------------------------")
+        # if(self.filter_contours_output):
+        #     print(self.filter_contours_output[0][0])
+        #     print("-------------------------")
+        # loop over the contours
+        for c in self.filter_contours_output:
+            # compute the center of the contour
+            M = cv2.moments(c)
+            cX = int(M["m10"] / M["m00"])
+            cY = int(M["m01"] / M["m00"])
+
+            # draw the contour and center of the shape on the image
+            cv2.drawContours(self.hsv_threshold_output, [c], -1, (0, 255, 0), 2)
+            cv2.circle(self.hsv_threshold_output, (cX, cY), 7, (128, 0, 0), -1)
+            cv2.putText(self.hsv_threshold_output, "center", (cX - 20, cY - 20),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+
+            # show the image
         cv2.imshow("HSV" , self.hsv_threshold_output)
 
     @staticmethod
